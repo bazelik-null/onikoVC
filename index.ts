f6a4464e8fa9203e5baf99525bf1822087e76502
bazelik-null <discordktdr@gmail.com>
2024-08-23 21:15:35 +0300

    finnaly work

--- index.ts
+++ index.ts
@@ -8,16 +8,15 @@ import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 
 export default definePlugin({
-    name: "oNiko",
+    name: "oniko",
     description: "Just Niko follows your mouse (Sprites from OneShot)",
     // Listing adryd here because this literally just evals her script
-    authors: [{ name: "bazelik", id: 1247930315603705898 }],
+    authors: [{ name: "bazelik143", id: 1247930315603705898 }],
 
     start() {
-        fetch("https://github.com/bazelik-null/oniko.js/blob/7bcf70a8cc57c48a98ebf9a2fc3378692cc511a2/oniko.js")
+        fetch("https://raw.githubusercontent.com/bazelik-null/oniko.js/7bcf70a8cc57c48a98ebf9a2fc3378692cc511a2/oniko.js")
             .then(x => x.text())
-            .then(s => s.replace("./oniko.png", "https://github.com/bazelik-null/oniko.js/blob/7bcf70a8cc57c48a98ebf9a2fc3378692cc511a2/oniko.png")
-                .replace("(isReducedMotion)", "(false)"))
+            .then(s => s.replace("./oniko.png", "https://raw.githubusercontent.com/bazelik-null/oniko.js/main/oniko.png"))
             .then(eval);
     },
 
